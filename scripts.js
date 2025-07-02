564869d88f64aeee5637a55f25d562bbc00cb4ea
Christabel Aidoo <113652194+christably@users.noreply.github.com>
2025-07-02 10:39:12 +0000

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 window.addEventListener('load', () => {
   setTimeout(() => {
     document.body.classList.add('switch');
-  }, 1000); // 1 second delay before switch
+  }, 1000); // auto-trigger transition
 });
